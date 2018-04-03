b87c5f2058d97bab2ed5afd7238f10c336442972
Rene Cienfuegos <rene@bandsintown.com>
2018-04-02 17:04:22 -0700

    Use fd for FZF

--- zsh/configs/general.zsh
+++ zsh/configs/general.zsh
@@ -1,5 +1,8 @@
 # Enable FZF
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+# export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='fd --type file --color=always --follow --hidden --exclude .git'
+export FZF_DEFAULT_OPTS="--ansi"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_TMUX=1
