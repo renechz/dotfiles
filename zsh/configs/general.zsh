b456a0e5b587ed865674755cf34efd13b65b46b5
René Cienfuegos <rene@bandsintown.com>
2019-05-29 16:03:11 -0700

    Simplify fzf zsh config

--- zsh/configs/general.zsh
+++ zsh/configs/general.zsh
@@ -1,4 +1,3 @@
-export FZF_DEFAULT_COMMAND='fd --type file --color=always --follow --hidden --exclude .git'
-export FZF_DEFAULT_OPTS="--ansi"
+export FZF_DEFAULT_COMMAND='fd --type file --follow --hidden --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_TMUX=1
